676a06427a8a38cbca9f70b55b8aca887c6c4afa
Sam <soham.mhatre97@gmail.com>
2024-03-10 14:42:23 -0400

    Created an connect to MongoDB

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com',
-    'https://stairchallenger',
-    'https://www.stairchallenger.com'
-]
+  "http://localhost:3000",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+  "https://stairchallenger",
+  "https://www.stairchallenger.com",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,36 +1,53 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const { logger } = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const PORT = process.env.PORT || 3500
+require("dotenv").config();
 
-app.use(logger)
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoonse = require("mongoose");
 
-app.use(cors(corsOptions))
+const PORT = process.env.PORT || 3500;
 
-app.use(cookieParser())
+console.log(process.env.NODE_ENV);
 
-app.use(express.json())
+connectDB();
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(logger);
 
-app.use('/', require('./routes/root'))
+app.use(cors(corsOptions));
 
-app.all('*',(req, res) => {
-    res.status(404)
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname,'views','404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found'})
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+app.use(cookieParser());
 
-app.use(errorHandler)
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+mongoonse.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoonse.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(`${err.no}:`);
+});
